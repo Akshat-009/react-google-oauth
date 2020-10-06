7290dd103487c078295b6d77200e9ff50489bad9
akshat <akshat_g@ch.iitr.ac.in>
2020-10-06 13:16:34 +0530

    Added basic template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import GoogleLogin from "react-google-login"
 function App() {
+  function responseGoogle(){
+
+  }
   return (
     <div>
-      
+      <GoogleLogin
+      clientId=""
+      buttonText="Login"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={"single_host_origin"}
+      />
     </div>
   )
 }
