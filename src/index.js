83f6ca8c460608655384c4f6b3cddee258572e9e
akshat <akshat_g@ch.iitr.ac.in>
2020-10-06 13:39:26 +0530

    Made some file changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import GoogleOauth from './GoogleOauth';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GoogleOauth />
   </React.StrictMode>,
   document.getElementById('root')
 );
